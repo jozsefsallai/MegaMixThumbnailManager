64bd00466d3c8585710161a94d3a1e52bd350c21
József Sallai <jozsef@sallai.me>
2022-11-27 19:24:03 +0200

    update assembly info and add mod template

--- MegaMixThumbnailManager.Core/Properties/AssemblyInfo.cs
+++ MegaMixThumbnailManager.Core/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MegaMixThumbnailManager.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Mega Mix Thumbnail Manager")]
+[assembly: AssemblyDescription("DLL that magically tries to fix your thumbnails in MM+.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MegaMixThumbnailManager.Core")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCompany("Kagamine Rin best girl")]
+[assembly: AssemblyProduct("Mega Mix Thumbnail Manager")]
+[assembly: AssemblyCopyright("Jozsef Sallai, licensed under MIT")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.2")]
+[assembly: AssemblyFileVersion("0.0.1.2")]
